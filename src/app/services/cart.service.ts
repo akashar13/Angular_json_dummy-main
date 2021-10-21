c0c7f993f6c479c9c9e0688dcace20d8f250abf5
Akash <akash.ar@frugalscientific.com>
2021-10-21 11:52:40 +0530

    fix

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -9,17 +9,24 @@ export class CartService {
   constructor(private http: HttpClient) { }
   checkout(desc: any, commen: any) {
     const url = "https://demo.wo.instio.co/customer/wo";
-    const data = {
-      "workOrderType": "REQUEST",
-      "priority": "10",
-      "service": "IRD Menu",
-      "addedByStaff":"false"
-    
-    }
-   
-    const ini="5c569717169aae1c9eb5a00a"
-    const com="5c419931169aae5fdadade33"
-   
-const headers = new HttpHeaders().set('Content-Type','application/json').set('customerId',"String")
-    return this.http.post(url, {data,comment:commen,description:desc,initiatedById:ini,companyId:com},{headers:headers})
-  }}
+
+
+    const ini = "5c569717169aae1c9eb5a00a"
+    const com = "5c419931169aae5fdadade33"
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        customerId: `5c569717169aae1c9eb5a00a`
+      })
+    };
+
+    return this.http.post(url, JSON.stringify({
+      workOrderType: `REQUEST`,
+      priority: 10,
+      service: `IRD Menu`,
+      addedByStaff: false,
+
+      comment: commen.itemname, description: desc.name, initiatedById: ini, companyId: com
+    }), httpOptions)
+  }
+}
